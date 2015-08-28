515127f6687170705fd4aed0f332862816102fa0
kgar <khaled.garbaya@gameduell.com>
2015-08-28 15:53:44 +0200

    input now attach itself to whatever set in the DUELLAppDelegate

--- backends/input_ios/project/src/InputCapturer.mm
+++ backends/input_ios/project/src/InputCapturer.mm
@@ -27,25 +27,18 @@
 #import <input_ios/InputCapturer.h>
 
 #import <input_ios/DUELLGestureRecognizer.h>
-
+#import "DUELLAppDelegate.h"
 @implementation InputCapturer
 
 + (void) initializeCapturer
 {
-	if ([UIApplication sharedApplication].keyWindow == nil)
+	DUELLAppDelegate *appDelegate = (DUELLAppDelegate *)[[UIApplication sharedApplication] delegate];
+	UIView *view = appDelegate.rootView;
+	if(view == nil)
 	{
-		@throw(@"Error: There is currently no window associated with this application. \
+		@throw(@"Error: There is currently no view set in the  DUELLAppDelegate. \
 			    Please initialize a library (e.g. opengl) that initializes a window, so the input library can attach to that.");
 	}
-
-	if ([UIApplication sharedApplication].keyWindow.rootViewController == nil)
-	{
-		@throw(@"Error: There is currently no root view controller associated with the key window. \
-			    Please initialize a library (e.g. opengl) that initializes a window, so the input library can attach to that.");
-	}
-
-	UIView *view = [UIApplication sharedApplication].keyWindow.rootViewController.view;
-
 	DUELLGestureRecognizer *recognizer = [[DUELLGestureRecognizer alloc] initWithTarget:nil action:nil];
     [view addGestureRecognizer:recognizer];
 

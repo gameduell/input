f5b687c2d453bd0e6042e72cfa75c1ffe8999a0b
Rui Campos <rui.campos@gameduell.de>
2015-12-07 18:18:11 +0100

    ugly hack to support the tvos remote as touch events

--- backends/input_ios/project/src/DUELLGestureRecognizer.mm
+++ backends/input_ios/project/src/DUELLGestureRecognizer.mm
@@ -32,10 +32,21 @@
 
 #include <hx/CFFI.h>
 
+#import <GameController/GameController.h>
+
 #define TOUCH_LIST_POOL_SIZE 40
 
 static NativeTouch *touchList = NULL;
 static int touchCount;
+static bool started = false;
+static float posX = 0.0f;
+static float posY = 0.0f;
+
+static float padValX = 0.0f;
+static float padValY = 0.0f;
+static GCMicroGamepad *microGamepad = NULL;
+
+#define SPEED_COEFFICIENT 10;
 
 DEFINE_KIND(k_TouchCount)
 DEFINE_KIND(k_TouchList)
@@ -43,7 +54,115 @@ DEFINE_KIND(k_TouchList)
 static value touchCountValue;
 static value touchListValue;
 
+static bool gestureRecognizerStarted = false;
+
+extern void callHaxeOnTouchesCallback(value touchCount, value touchList);
+
 @implementation DUELLGestureRecognizer
+
+
++ (void) load
+{
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(handleControllerDidConnectNotification:) name:GCControllerDidConnectNotification object:nil];
+
+}
++ (void)handleControllerDidConnectNotification:(NSNotification *)notification {
+
+    GCController *gameController = notification.object;
+    [self registerCharacterMovementEvents:gameController];
+}
+
++ (void)registerCharacterMovementEvents:(GCController *)gameController {
+
+    // An analog movement handler for D-pads and thumbsticks.
+    __weak typeof(self) weakSelf = self;
+    GCControllerDirectionPadValueChangedHandler movementHandler = ^(GCControllerDirectionPad *dpad, float xValue, float yValue) {
+
+
+        padValX = xValue;
+        padValY = yValue;
+
+    };
+
+    // Apple TV remote
+    microGamepad = gameController.microGamepad;
+    // Allow the gamepad to handle transposing D-pad values when rotating the controller.
+    microGamepad.allowsRotation = YES;
+    microGamepad.dpad.valueChangedHandler = movementHandler;
+
+
+    // Gamepad D-pad
+    GCGamepad *gamepad = gameController.gamepad;
+    gamepad.dpad.valueChangedHandler = movementHandler;
+
+    // Extended gamepad left thumbstick
+    GCExtendedGamepad *extendedGamepad = gameController.extendedGamepad;
+    extendedGamepad.leftThumbstick.valueChangedHandler = movementHandler;
+
+}
+
+
+
+- (void) sendPressTouches
+{
+    if (!gestureRecognizerStarted)
+        return;
+
+    if (!microGamepad)
+        return;
+
+    touchCount = 1;
+
+    touchList[0].id = 0;
+
+    posX += padValX * SPEED_COEFFICIENT;
+    posY += - padValY * SPEED_COEFFICIENT;
+
+    if (posX >= [UIScreen mainScreen].nativeBounds.size.width)
+    {
+        posX = [UIScreen mainScreen].nativeBounds.size.width;
+    }
+
+    if (posX < 0)
+    {
+        posX = 0;
+    }
+
+    if (posY >= [UIScreen mainScreen].nativeBounds.size.height)
+    {
+        posY = [UIScreen mainScreen].nativeBounds.size.height;
+    }
+
+    if (posY < 0)
+    {
+        posY = 0;
+    }
+
+    if (!started && !microGamepad.buttonA.pressed)
+    {
+        touchList[0].state = 4;
+    }
+    else if (!started && microGamepad.buttonA.pressed)
+    {
+        started = true;
+        touchList[0].state = 0;
+    }
+    else if (started && !microGamepad.buttonA.pressed)
+    {
+        started = false;
+        touchList[0].state = 3;
+    }
+    else
+    {
+        touchList[0].state = 2;
+    }
+
+    touchList[0].x = posX;
+    touchList[0].y = posY;
+
+    callHaxeOnTouchesCallback(touchCountValue, touchListValue);
+}
+
 
 extern void callSetCachedVariablesCallback(value touchCount, value touchList);
 - (void) initializeTouchCapturing
@@ -54,6 +173,12 @@ extern void callSetCachedVariablesCallback(value touchCount, value touchList);
     touchListValue = alloc_abstract(k_TouchList, touchList);
 
     callSetCachedVariablesCallback(touchCountValue, touchListValue);
+    gestureRecognizerStarted = true;
+    posX = [UIScreen mainScreen].nativeBounds.size.width * 0.5;
+    posY = [UIScreen mainScreen].nativeBounds.size.height * 0.5;
+
+    NSTimer* timer = [NSTimer timerWithTimeInterval:0.01f target:self selector:@selector(sendPressTouches) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -84,7 +209,6 @@ int convertPointerToUniqueInt(UITouch *touch)
     return hash;
 }
 
-extern void callHaxeOnTouchesCallback(value touchCount, value touchList);
 - (void) dispatchTouches:(NSSet *)touches
 {
     touchCount = touches.count;
